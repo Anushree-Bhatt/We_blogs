840dd9c22f8689cf793fd671bb474c8454a7ff88
Anushree <anushreebhatt357@gmail.com>
2025-06-18 15:30:18 -0400

    created post entity with required fields and annotations

---
+++ src/main/java/net/practice_mvc/WeBlogs/entity/Post.java
@@ -0,0 +1,32 @@
+package net.practice_mvc.WeBlogs.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "posts", schema = "blogApp")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+    private String url;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+
+}
